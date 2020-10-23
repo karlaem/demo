63e2becb72fe26097fba8d73d3330798dc94b631
karlaem <60242444+karlaem@users.noreply.github.com>
2020-10-22 17:34:13 -0700

    Made my file

--- index.js
+++ index.js
@@ -3,37 +3,18 @@ console.log("this is my first API application");
 const express = require("express");
 const app = express();
 const port = 3000;
-//let because data changes
-let data = require("./data.json");
 
-//get to do list data
-function getTodos(req, res){
-    let response = { todos: data};
-    res.send(response);
-}
 
-/*//get request, respond
+//get request, respond
 function reqGet(req, res){
     //makes json data
-    let response = { output: "Hello World"};
+    let response = { firstName: "Karla Estefania", lastName: "Mercado Garcia", output: "Hello World"};
     res.send(response);
 }
 
-//get User
-function reqGetUser(req, res){
-    //req is the get variables
-    //localhost:3000/user?name=Akashjot&country=canada
-    let name = req.query.name;
-    let country = req.query.country;
-    let response ={ name:name, country:country};
-    //console.log(req);
-    res.send(response);
-}*/
 
-//app.get("/", reqGet);//default request
-//app.get("/user", reqGetUser);
+app.get("/", reqGet);//default request
 
-app.get("/", getTodos);
 
 function appStart(){
     console.log("To-Do-Api listening at "+ port);
